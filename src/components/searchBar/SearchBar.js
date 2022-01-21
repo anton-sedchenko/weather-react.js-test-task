e243df5e32dd07957d81f622bce13088be6a5c14
Anton Sedchenko <anton.sedchenko@gmail.com>
2022-01-21 02:41:19 +0200

    autocomplete when searching city implemented

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -7,18 +7,56 @@ import { fetchWeather } from "../../actions/weather";
 const SearchBar = () => {
     const dispatch = useDispatch();
     const [inputValue, setInputValue] = useState('');
+    const [selectedCities, setSelectedCities] = useState(JSON.parse(localStorage.getItem('selectedCities')) || ['Lisbon']);
+    const onAutocompleteItemClick = (e) => {
+        setInputValue(e.target.textContent);
+        dispatch(fetchWeather(e.target.textContent));
+        setInputValue('');
+    }
 
     return (
-        <div className="header__search-bar">
-            <input
-                className="header__search-bar-input"
-                onChange={(event) => setInputValue(event.target.value)}
-                value={inputValue}
-            />
+        <div className="header__search-container">
+            <div className="header__search-bar-wrapper">
+                <input
+                    type="text"
+                    className="header__search-bar"
+                    value={inputValue}
+                    onChange={(event) => {setInputValue(event.target.value)}}
+                />
+                <ul className="autocomplete">
+                    {
+                        inputValue
+                        ? selectedCities.map((cityName, i) => {
+                            return (
+                                <li
+                                    className="autocomplete__item"
+                                    key={i}
+                                    onClick={onAutocompleteItemClick}
+                                >
+                                    {cityName}
+                                </li>
+                            )
+                        })
+                        : null
+                    }
+                </ul>
+            </div>
             <Button
                 className="header__search-bar-button"
                 type="primary" size="large"
-                onClick={() => dispatch(fetchWeather(inputValue))}
+                onClick={() => {
+                    setSelectedCities((selectedCities) => {
+                        if (selectedCities.some((item) => item === inputValue)) {
+                            return selectedCities;
+                        }
+                        localStorage.setItem('selectedCities', JSON.stringify([...selectedCities, inputValue]));
+
+                        return [...selectedCities, inputValue];
+                    });
+
+                    dispatch(fetchWeather(inputValue));
+                    setInputValue('');
+                }}
             >
                 Add
             </Button>
