e913be7cd46b49804df375725161c21a7e4ac552
Anton Sedchenko <anton.sedchenko@gmail.com>
2022-01-24 02:44:53 +0200

    code refactoring

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -68,21 +68,27 @@ const Card = (cardData) => {
                 </div>
             </div>
             <div className="graph">
-                <div style={{ width: '100%', height: '100%' }}>
+                <div style={{ width: '100%', height: '120px' }}>
                     <ResponsiveContainer>
                         <AreaChart
                             data={graphData}
                             margin={{
-                                top: 10,
-                                right: 30,
+                                top: 0,
+                                right: 0,
                                 left: 0,
                                 bottom: 0,
                             }}
+                            layout="horizontal"
                         >
-                            <CartesianGrid strokeDasharray="3 3" />
+                            <defs>
+                                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+                                </linearGradient>
+                            </defs>
                             <XAxis dataKey="name" />
                             <Tooltip />
-                            <Area type="monotone" dataKey="temp" stroke="#8884d8" fill="#8884d8" />
+                            <Area type="monotone" dataKey="temp" stroke="none" fillOpacity={1} fill="url(#colorUv)" />
                         </AreaChart>
                     </ResponsiveContainer>
                 </div>
